528c5c4ddeb01d71e24d4feec7cfa599336d8f66
Asterios Raptis <asterios.raptis@gmx.net>
2016-07-22 11:40:26 +0200

    Created new extension methods for arrays. parent version upgrade

--- src/main/java/de/alpharogroup/collections/ArrayExtensions.java
+++ src/main/java/de/alpharogroup/collections/ArrayExtensions.java
@@ -1,5 +1,7 @@
 package de.alpharogroup.collections;
 
+import java.util.Arrays;
+
 /**
  * The class {@link ArrayExtensions} is an extensions class for use with array objects.
  */
@@ -7,19 +9,214 @@ public final class ArrayExtensions
 {
 
 	/**
-	 * Gets the first object from the given List.
+	 * Gets the first object from the given array.
 	 *
 	 * @param <T>
 	 *            the generic type
-	 * @param list
-	 *            the List.
-	 * @return Returns the first object from the given List or null if the List is empty or null.
+	 * @param array
+	 *            the array.
+	 * @return Returns the first object from the given array or null if the array is empty or null.
 	 */
 	public static <T> T getFirst(final T[] array)
 	{
-		if(array != null && array.length != 0) {
+		if (array != null && array.length != 0)
+		{
 			return array[0];
 		}
 		return null;
 	}
+
+	/**
+	 * Gets the index of the given element in the given array.
+	 *
+	 * @param <T>
+	 *            the generic type
+	 * @param array
+	 *            the array
+	 * @param element
+	 *            the element
+	 * @return the int
+	 */
+	public static <T> int getIndex(final T[] array, final T element)
+	{
+		return ArrayExtensions.indexOf(array, element);
+	}
+
+	/**
+	 * Gets the last object from the given array.
+	 *
+	 * @param <T>
+	 *            the generic type
+	 * @param array
+	 *            the array.
+	 * @return Returns the last object from the given array or null if the array is empty or null.
+	 */
+	public static <T> T getLast(final T[] array)
+	{
+		if (array != null && array.length != 0)
+		{
+			final int lastIndex = array.length - 1;
+			return array[lastIndex];
+		}
+		return null;
+	}
+
+	/**
+	 * Gets the next index of the given array.
+	 *
+	 * @param <T>
+	 *            the generic type
+	 * @param array
+	 *            the array
+	 * @param element
+	 *            the element
+	 * @return the next index
+	 */
+	public static <T> int getNextIndex(final T[] array, final T element)
+	{
+		final int lastIndex = array.length - 1;
+		final int indexOfElement = ArrayExtensions.indexOf(array, element);
+		if (indexOfElement == -1)
+		{
+			return indexOfElement;
+		}
+		if (indexOfElement == lastIndex)
+		{
+			return 0;
+		}
+		final int nextIndex = indexOfElement + 1;
+		return nextIndex;
+	}
+
+	/**
+	 * Gets the next indexes from the given count of the given array.
+	 *
+	 * @param <T>
+	 *            the generic type
+	 * @param array
+	 *            the array
+	 * @param element
+	 *            the element
+	 * @param count
+	 *            the count
+	 * @return the next indexes
+	 */
+	public static <T> int[] getNextIndexes(final T[] array, final T element, final int count)
+	{
+		final int[] nextIndexes = new int[count];
+		T currentElement = element;
+		for (int i = 0; i < count; i++)
+		{
+			final int nextIndex = getNextIndex(array, currentElement);
+			nextIndexes[i] = nextIndex;
+			currentElement = array[nextIndex];
+		}
+		return nextIndexes;
+	}
+
+	/**
+	 * Gets the previous index of the given array.
+	 *
+	 * @param <T>
+	 *            the generic type
+	 * @param array
+	 *            the array
+	 * @param element
+	 *            the element
+	 * @return the previous index
+	 */
+	public static <T> int getPreviousIndex(final T[] array, final T element)
+	{
+		final int lastIndex = array.length - 1;
+		final int indexOfElement = ArrayExtensions.indexOf(array, element);
+		if (indexOfElement == -1)
+		{
+			return indexOfElement;
+		}
+		if (indexOfElement == 0)
+		{
+			return lastIndex;
+		}
+		final int previousIndex = indexOfElement - 1;
+		return previousIndex;
+	}
+
+	/**
+	 * Gets the previous indexes from the given count of the given array.
+	 *
+	 * @param <T>
+	 *            the generic type
+	 * @param array
+	 *            the array
+	 * @param element
+	 *            the element
+	 * @param count
+	 *            the count
+	 * @return the previous indexes
+	 */
+	public static <T> int[] getPreviousIndexes(final T[] array, final T element, final int count)
+	{
+		final int[] previousIndexes = new int[count];
+		T currentElement = element;
+		for (int i = 0; i < count; i++)
+		{
+			final int previousIndex = getPreviousIndex(array, currentElement);
+			previousIndexes[i] = previousIndex;
+			currentElement = array[previousIndex];
+		}
+		return previousIndexes;
+	}
+
+	/**
+	 * Gets the index of the given element in the given array.
+	 *
+	 * @param <T>
+	 *            the generic type
+	 * @param array
+	 *            the array
+	 * @param element
+	 *            the element
+	 * @return the int
+	 */
+	public static <T> int indexOf(final T[] array, final T element)
+	{
+		final int indexOfElement = Arrays.asList(array).indexOf(element);
+		return indexOfElement;
+	}
+
+	/**
+	 * Checks if the given element is the first in the given array.
+	 *
+	 * @param <T>
+	 *            the generic type
+	 * @param array
+	 *            the array
+	 * @param element
+	 *            the element
+	 * @return true if the given element is the first otherwise false
+	 */
+	public static <T> boolean isFirst(final T[] array, final T element)
+	{
+		final int indexOfElement = Arrays.asList(array).indexOf(element);
+		return indexOfElement == 0;
+	}
+
+	/**
+	 * Checks if the given element is the last in the given array.
+	 *
+	 * @param <T>
+	 *            the generic type
+	 * @param array
+	 *            the array
+	 * @param element
+	 *            the element
+	 * @return true if the given element is the last otherwise false
+	 */
+	public static <T> boolean isLast(final T[] array, final T element)
+	{
+		final int lastIndex = array.length - 1;
+		final int indexOfElement = Arrays.asList(array).indexOf(element);
+		return indexOfElement == lastIndex;
+	}
+
 }
