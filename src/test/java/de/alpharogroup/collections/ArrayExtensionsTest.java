83ac0babab10e527a5647c6baebe095550c31d23
Asterios Raptis <asterios.raptis@gmx.net>
2016-12-29 13:22:52 +0100

    warnings removed

--- src/test/java/de/alpharogroup/collections/ArrayExtensionsTest.java
+++ src/test/java/de/alpharogroup/collections/ArrayExtensionsTest.java
@@ -24,13 +24,9 @@
  */
 package de.alpharogroup.collections;
 
-import java.util.Date;
-
 import org.testng.AssertJUnit;
 import org.testng.annotations.Test;
 
-import de.alpharogroup.date.CalculateDateExtensions;
-import de.alpharogroup.date.CreateDateExtensions;
 import lombok.experimental.ExtensionMethod;
 
 /**
@@ -327,26 +323,25 @@ public class ArrayExtensionsTest
 	public void testSplit()
 	{
 		final String byteString = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.";
-//		final StringBuilder sb = new StringBuilder();
-//		for (int i = 0; i < 1000000; i++)
-//		{
-//			sb.append(byteString);
-//		}
-//		byteString = sb.toString();
+		// final StringBuilder sb = new StringBuilder();
+		// for (int i = 0; i < 1000000; i++)
+		// {
+		// sb.append(byteString);
+		// }
+		// byteString = sb.toString();
 		final byte[] byteArray = byteString.getBytes();
-		Date start = null;
-		Date end = null;
-		long elapsed;
+		// Date start = null;
+		// Date end = null;
+		// long elapsed;
 
-		 AssertJUnit.assertEquals(byteArray.length, 56);
+		AssertJUnit.assertEquals(byteArray.length, 56);
 		// simple benchmark start
-		start = CreateDateExtensions.now();
+		// start = CreateDateExtensions.now();
 		final byte[][] splitedBytes = ArrayExtensions.splitInChunks(byteArray, 5);
-		end = CreateDateExtensions.now();
+		// end = CreateDateExtensions.now();
 		// simple benchmark end
-		elapsed = CalculateDateExtensions.calculateElapsedTime(start, end);
-		System.out.println("ArrayExtensions.split:"+elapsed);
-		 AssertJUnit.assertEquals(splitedBytes.length, 12);
+		// elapsed = CalculateDateExtensions.calculateElapsedTime(start, end);
+		AssertJUnit.assertEquals(splitedBytes.length, 12);
 
 	}
 
