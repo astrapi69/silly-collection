7b7c6899d87017380cb01bb43832e6ada07bb8e0
James Phelps <tyler.durden69@gmx.de>
2017-09-20 08:55:14 +0200

    Update CheckTest.java

--- src/test/java/de/alpharogroup/check/CheckTest.java
+++ src/test/java/de/alpharogroup/check/CheckTest.java
@@ -22,101 +22,162 @@
  * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
  * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
-package de.alpharogroup.comparators;
+package de.alpharogroup.check;
 
-import org.testng.AssertJUnit;
-import org.testng.annotations.Test;
+import static org.junit.Assert.assertNotNull;
 
-import de.alpharogroup.test.objects.Person;
-import lombok.experimental.ExtensionMethod;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 /**
- * The test class {@link ComparatorExtensions}.
+ * Test class for the class {@link Check}.
  */
-@ExtensionMethod({ ComparatorExtensions.class })
-public class ComparatorExtensionsTest
+public class CheckTest
 {
+	/**
+	 * A rule for expecting exceptions
+	 */
+	@Rule
+	public ExpectedException throwable = ExpectedException.none();
 
 	/**
-	 * Test for method {@link ComparatorExtensions#compare(Comparable, Comparable)}.
+	 * Test of get method, of class Check.
 	 */
 	@Test
-	public void testCompare()
+	public void testGet()
 	{
-		final Person person = Person.builder().name("al").build();
-		final Person otherPerson = Person.builder().name("bert").build();
-		final int actual = person.compare(otherPerson);
-		AssertJUnit.assertTrue(actual == -1);
+		final Check actual = Check.get();
+		assertNotNull(actual);
 	}
 
 	/**
-	 * Test for method {@link ComparatorExtensions#compare(Comparable, Comparable, SortOrder)}.
+	 * Test for {@link Check#isInRange(Double, Double, Double, String)}
 	 */
 	@Test
-	public void testCompareSortOrder()
+	public void testIsInRangeDouble()
 	{
-		final Person person = Person.builder().name("al").build();
-		final Person otherPerson = Person.builder().name("bert").build();
-		int actual = person.compare(otherPerson, SortOrder.DESCENDING);
-		AssertJUnit.assertTrue(actual == 1);
-		actual = person.compare(otherPerson, SortOrder.ASCENDING);
-		AssertJUnit.assertTrue(actual == -1);
+		final Double min = 0.0d;
+		final Double max = 5.0d;
+		final Double value = 6.0d;
+		final String name = "parameter";
+		throwable.expect(IllegalArgumentException.class);
+		throwable.expectMessage("Given argument '" + name + "' should have a value between " + min
+			+ " - " + max + ", but given argument is currently:" + value + "");
+
+		Check.get().isInRange(min, max, value, name);
 	}
 
 	/**
-	 * Test for method {@link ComparatorExtensions#nullCheck(Object, Object)}.
+	 * Test for {@link Check#isInRange(Float, Float, Float, String)}
 	 */
 	@Test
-	public void testNullCheck()
+	public void testIsInRangeFloat()
 	{
-		Integer actual = ComparatorExtensions.nullCheck(Person.builder().build(), null);
-		AssertJUnit.assertTrue(actual == 1);
-		Person person = Person.builder().build();
-		actual = person.nullCheck(null);
-		AssertJUnit.assertTrue(actual == 1);
-		final Person otherPerson = Person.builder().name("s").build();
-		actual = person.nullCheck(otherPerson);
-		AssertJUnit.assertTrue(actual == null);
-		person = null;
-		actual = person.nullCheck(null);
-		AssertJUnit.assertTrue(actual == 0);
-		actual = person.nullCheck(otherPerson);
-		AssertJUnit.assertTrue(actual == -1);
+		final Float min = 0.0f;
+		final Float max = 5.0f;
+		final Float value = 6.0f;
+		final String name = "parameter";
+		throwable.expect(IllegalArgumentException.class);
+		throwable.expectMessage("Given argument '" + name + "' should have a value between " + min
+			+ " - " + max + ", but given argument is currently:" + value + "");
+
+		Check.get().isInRange(min, max, value, name);
 	}
 
 	/**
-	 * Test for method {@link ComparatorExtensions#nullCheck(Object, Object, boolean)}.
+	 * Test for {@link Check#isInRange(Integer, Integer, Integer, String)}
 	 */
 	@Test
-	public void testNullCheckWithNullFlag()
+	public void testIsInRangeInteger()
 	{
-		// false case...
-		Integer actual = ComparatorExtensions.nullCheck(Person.builder().build(), null, false);
-		AssertJUnit.assertTrue(actual == 1);
-		Person person = Person.builder().build();
-		actual = person.nullCheck(null, false);
-		AssertJUnit.assertTrue(actual == 1);
-		final Person otherPerson = Person.builder().name("s").build();
-		actual = person.nullCheck(otherPerson, false);
-		AssertJUnit.assertTrue(actual == null);
-		person = null;
-		actual = person.nullCheck(null, false);
-		AssertJUnit.assertTrue(actual == 0);
-		actual = person.nullCheck(otherPerson, false);
-		AssertJUnit.assertTrue(actual == -1);
-		// true case...
-		actual = ComparatorExtensions.nullCheck(Person.builder().build(), null, true);
-		AssertJUnit.assertTrue(actual == -1);
-		person = Person.builder().build();
-		actual = person.nullCheck(null, true);
-		AssertJUnit.assertTrue(actual == -1);
-		actual = person.nullCheck(otherPerson, true);
-		AssertJUnit.assertTrue(actual == null);
-		person = null;
-		actual = person.nullCheck(null, true);
-		AssertJUnit.assertTrue(actual == 0);
-		actual = person.nullCheck(otherPerson, true);
-		AssertJUnit.assertTrue(actual == 1);
+		final Integer min = 0;
+		final Integer max = 5;
+		final Integer value = 6;
+		final String name = "parameter";
+		throwable.expect(IllegalArgumentException.class);
+		throwable.expectMessage("Given argument '" + name + "' should have a value between " + min
+			+ " - " + max + ", but given argument is currently:" + value + "");
+
+		Check.get().isInRange(min, max, value, name);
+	}
+
+	/**
+	 * Test for {@link Check#isInRange(Long, Long, Long, String)}
+	 */
+	@Test
+	public void testIsInRangeLong()
+	{
+		final Long min = 0l;
+		final Long max = 5l;
+		final Long value = 6l;
+		final String name = "parameter";
+		throwable.expect(IllegalArgumentException.class);
+		throwable.expectMessage("Given argument '" + name + "' should have a value between " + min
+			+ " - " + max + ", but given argument is currently:" + value + "");
+
+		Check.get().isInRange(min, max, value, name);
+	}
+
+	/**
+	 * Test for {@link Check#notEmpty(java.util.Collection, String)}
+	 */
+	@Test
+	public void testNotEmptyCollection()
+	{
+		final String name = "list";
+		final List<String> list = new ArrayList<>();
+		throwable.expect(IllegalArgumentException.class);
+		throwable.expectMessage("Given collection '" + name + "' may not be empty.");
+
+		Check.get().notEmpty(list, name);
+	}
+
+
+	/**
+	 * Test for {@link Check#notEmpty(java.util.Collection, String)}
+	 */
+	@Test
+	public void testNotEmptyMap()
+	{
+		final String name = "map";
+		final Map<String, String> emptyMap = new HashMap<>();
+		throwable.expect(IllegalArgumentException.class);
+		throwable.expectMessage("Given map '" + name + "' may not be empty.");
+
+		Check.get().notEmpty(emptyMap, name);
+	}
+
+	/**
+	 * Test for {@link Check#notEmpty(CharSequence, String)}
+	 */
+	@Test
+	public void testNotEmptyString()
+	{
+		final String name = "parameter";
+		final String argument = "";
+		throwable.expect(IllegalArgumentException.class);
+		throwable.expectMessage("Given argument '" + name + "' may not be empty.");
+
+		Check.get().notEmpty(argument, name);
+	}
+
+	/**
+	 * Test for {@link Check#notNull(Object, String)}
+	 */
+	@Test
+	public void testNotNull()
+	{
+		final String name = "parameter";
+		throwable.expect(IllegalArgumentException.class);
+		throwable.expectMessage("Given argument '" + name + "' may not be null.");
+
+		Check.get().notNull(null, name);
 	}
 
 }
