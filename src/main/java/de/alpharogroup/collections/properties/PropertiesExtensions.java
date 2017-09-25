c0e03a98b191160dd77f346ae5d135a5a8460279
James Phelps <tyler.durden69@gmx.de>
2017-09-25 18:46:34 +0200

    Update PropertiesExtensions.java

--- src/main/java/de/alpharogroup/collections/properties/PropertiesExtensions.java
+++ src/main/java/de/alpharogroup/collections/properties/PropertiesExtensions.java
@@ -22,7 +22,7 @@
  * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
  * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
-package de.alpharogroup.resourcebundle.properties;
+package de.alpharogroup.collections.properties;
 
 import java.io.File;
 import java.io.FileInputStream;
