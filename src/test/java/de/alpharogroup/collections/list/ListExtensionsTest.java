4493414f834be4e5bcdc118b42a0ca5dfafffc7a
James Phelps <tyler.durden69@gmx.de>
2018-01-24 10:50:42 +0100

    Update ListExtensionsTest.java

--- src/test/java/de/alpharogroup/collections/list/ListExtensionsTest.java
+++ src/test/java/de/alpharogroup/collections/list/ListExtensionsTest.java
@@ -32,6 +32,7 @@ import static org.testng.AssertJUnit.assertTrue;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
