0912ee4e5de63defe3cdb7bdc59bd198a899baee
James Phelps <tyler.durden69@gmx.de>
2018-01-29 18:58:47 +0100

    Update KeyValuePairKeyComparatorTest.java

--- src/test/java/de/alpharogroup/comparators/pairs/KeyValuePairKeyComparatorTest.java
+++ src/test/java/de/alpharogroup/comparators/pairs/KeyValuePairKeyComparatorTest.java
@@ -24,25 +24,59 @@
  */
 package de.alpharogroup.comparators.pairs;
 
+import static org.testng.Assert.assertTrue;
+import static org.testng.AssertJUnit.assertNotSame;
+
 import org.meanbean.test.BeanTester;
 import org.testng.annotations.Test;
 
-import de.alpharogroup.collections.pairs.ValueBox;
-
+import de.alpharogroup.collections.pairs.KeyValuePair;
+import java.util.Comparator;
 
 /**
  * The unit test class for the class {@link KeyValuePairKeyComparator}.
  */
 public class KeyValuePairKeyComparatorTest
 {
+	boolean expected;
+	int actual;
+		
+	/**
+	 * Test method for {@link KeyValuePairKeyComparator#compare(Object, Object)}
+	 */
+	@Test
+	public void testCompare()
+	{
+		KeyValuePair<String, String> o1 = KeyValuePair.<String, String> builder().key("1")
+				.value("novalue").build();
+		KeyValuePair<String, String> o2 = new KeyValuePair<>();
+		o2.setKey("2");
+		o2.setValue("somevalue");
+		
+		assertNotSame(expected, actual);
+		
+		Comparator<KeyValuePair<String, String>> comparator =  new KeyValuePairKeyComparator<>();
+		
+		actual = comparator.compare(o1, o1);
+		expected = actual == 0;
+		assertTrue(expected);
+		
+		actual = comparator.compare(o1, o2);
+		expected = 0 > actual;
+		assertTrue(expected);
+		
+		actual = comparator.compare(o2, o1);
+		expected = 0 < actual;
+		assertTrue(expected);
+	}
 
 	/**
-	 * Test method for {@link ValueBox}
+	 * Test method for {@link KeyValuePairKeyComparator}
 	 */
 	@Test
 	public void testWithBeanTester()
 	{
 		BeanTester beanTester = new BeanTester();
-		beanTester.testBean(ValueBox.class);
+		beanTester.testBean(KeyValuePairKeyComparator.class);
 	}
 }
