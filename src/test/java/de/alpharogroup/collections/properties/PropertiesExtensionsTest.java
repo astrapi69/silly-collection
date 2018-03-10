88ad60fc1664893d5765f38686eef56a57bb51e1
Asterios Raptis <asterios.raptis@gmx.net>
2018-03-10 12:13:45 +0100

    organize imports

--- src/test/java/de/alpharogroup/collections/properties/PropertiesExtensionsTest.java
+++ src/test/java/de/alpharogroup/collections/properties/PropertiesExtensionsTest.java
@@ -32,6 +32,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.lang.reflect.InvocationTargetException;
 import java.net.URL;
