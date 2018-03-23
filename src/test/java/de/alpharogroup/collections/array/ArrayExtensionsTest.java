8084627f9b3248e21c2e8d09e091d16bb6dedc32
Tatjana Kopalova <tatjana19@users.noreply.github.com>
2018-03-23 15:35:45 +0100

    Update ArrayExtensionsTest.java

--- src/test/java/de/alpharogroup/collections/array/ArrayExtensionsTest.java
+++ src/test/java/de/alpharogroup/collections/array/ArrayExtensionsTest.java
@@ -28,6 +28,7 @@ import static org.testng.AssertJUnit.assertEquals;
 
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+import java.util.Arrays;
 
 import org.meanbean.test.BeanTestException;
 import org.meanbean.test.BeanTester;
@@ -58,7 +59,7 @@ public class ArrayExtensionsTest
 		expected = ArrayExtensions.newArray(5, 6, 7, 8, 9);
 		assertTrue(Arrays.deepEquals(actual, expected));
 
-		actual = ListExtensions.newRangeArray(1, 49);
+		actual = ArrayExtensions.newRangeArray(1, 49);
 		expected = ArrayExtensions.newArray(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16,
 			17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38,
 			39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49);
