785f99817db80c9613d26d840ff298011fe6b627
Asterios Raptis <asterios.raptis@gmx.net>
2018-04-02 16:19:12 +0200

    formatted, javadoc, sort members and organize imports

--- src/main/java/de/alpharogroup/collections/map/MapExtensions.java
+++ src/main/java/de/alpharogroup/collections/map/MapExtensions.java
@@ -3,24 +3,20 @@
  *
  * Copyright (C) 2015 Asterios Raptis
  *
- * Permission is hereby granted, free of charge, to any person obtaining
- * a copy of this software and associated documentation files (the
- * "Software"), to deal in the Software without restriction, including
- * without limitation the rights to use, copy, modify, merge, publish,
- * distribute, sublicense, and/or sell copies of the Software, and to
- * permit persons to whom the Software is furnished to do so, subject to
- * the following conditions:
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+ * associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
  *
- * The above copyright notice and this permission notice shall be
- * included in all copies or substantial portions of the Software.
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
  *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
- * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
- * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
- * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
- * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+ * NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 package de.alpharogroup.collections.map;
 
@@ -58,8 +54,8 @@ public final class MapExtensions
 	 *            the generic type of the value
 	 *
 	 * @return The new {@link InsertionOrderMap}
-	 * @deprecated use instead the same name method from <code>MapFactory</code>
-	 * Note: will be removed with the next minor release
+	 * @deprecated use instead the same name method from <code>MapFactory</code> Note: will be
+	 *             removed with the next minor release
 	 */
 	public static <K, V> Map<K, V> newInsertionOrderMap()
 	{
@@ -147,8 +143,8 @@ public final class MapExtensions
 	 * </pre>
 	 *
 	 * @return The {@link java.util.Map} that acts like a javascript associative array.
-	 * @deprecated use instead the same name method from <code>MapFactory</code>
-	 * Note: will be removed with the next minor release
+	 * @deprecated use instead the same name method from <code>MapFactory</code> Note: will be
+	 *             removed with the next minor release
 	 */
 	public static Map<Integer, Map<String, String>> newAssosiativeArrayMap()
 	{
@@ -164,8 +160,8 @@ public final class MapExtensions
 	 *            the generic type of the value
 	 *
 	 * @return The new {@link ConcurrentHashMap}.
-	 * @deprecated use instead the same name method from <code>MapFactory</code>
-	 * Note: will be removed with the next minor release
+	 * @deprecated use instead the same name method from <code>MapFactory</code> Note: will be
+	 *             removed with the next minor release
 	 */
 	public static <K, V> ConcurrentHashMap<K, V> newConcurrentHashMap()
 	{
@@ -182,8 +178,8 @@ public final class MapExtensions
 	 * @param initialCapacity
 	 *            the initial capacity
 	 * @return The new {@link ConcurrentHashMap}.
-	 * @deprecated use instead the same name method from <code>MapFactory</code>
-	 * Note: will be removed with the next minor release
+	 * @deprecated use instead the same name method from <code>MapFactory</code> Note: will be
+	 *             removed with the next minor release
 	 */
 	public static <K, V> ConcurrentHashMap<K, V> newConcurrentHashMap(final int initialCapacity)
 	{
@@ -199,8 +195,8 @@ public final class MapExtensions
 	 *            the generic type of the value
 	 *
 	 * @return The new {@link HashMap}.
-	 * @deprecated use instead the same name method from <code>MapFactory</code>
-	 * Note: will be removed with the next minor release
+	 * @deprecated use instead the same name method from <code>MapFactory</code> Note: will be
+	 *             removed with the next minor release
 	 */
 	public static <K, V> Map<K, V> newHashMap()
 	{
@@ -216,8 +212,8 @@ public final class MapExtensions
 	 *            the generic type of the value
 	 *
 	 * @return The new {@link LinkedHashMap}.
-	 * @deprecated use instead the same name method from <code>MapFactory</code>
-	 * Note: will be removed with the next minor release
+	 * @deprecated use instead the same name method from <code>MapFactory</code> Note: will be
+	 *             removed with the next minor release
 	 */
 	public static <K, V> Map<K, V> newLinkedHashMap()
 	{
@@ -235,8 +231,8 @@ public final class MapExtensions
 	 *            the initial capacity
 	 *
 	 * @return The new {@link LinkedHashMap}.
-	 * @deprecated use instead the same name method from <code>MapFactory</code>
-	 * Note: will be removed with the next minor release
+	 * @deprecated use instead the same name method from <code>MapFactory</code> Note: will be
+	 *             removed with the next minor release
 	 */
 	public static <K, V> Map<K, V> newLinkedHashMap(final int initialCapacity)
 	{
@@ -253,8 +249,8 @@ public final class MapExtensions
 	 * @param initialCapacity
 	 *            the initial capacity
 	 * @return The new {@link HashMap}.
-	 * @deprecated use instead the same name method from <code>MapFactory</code>
-	 * Note: will be removed with the next minor release
+	 * @deprecated use instead the same name method from <code>MapFactory</code> Note: will be
+	 *             removed with the next minor release
 	 */
 	public static <K, V> Map<K, V> newHashMap(final int initialCapacity)
 	{
@@ -271,8 +267,8 @@ public final class MapExtensions
 	 *            the generic type of the value
 	 *
 	 * @return The new {@link LazyMap}.
-	 * @deprecated use instead the same name method from <code>MapFactory</code>
-	 * Note: will be removed with the next minor release
+	 * @deprecated use instead the same name method from <code>MapFactory</code> Note: will be
+	 *             removed with the next minor release
 	 */
 	@SuppressWarnings({ "unchecked", "rawtypes" })
 	public static <K, V> Map<K, V> newLazyMap()
@@ -290,8 +286,8 @@ public final class MapExtensions
 	 *            the generic type of the value
 	 *
 	 * @return The new {@link LazyMap}.
-	 * @deprecated use instead the same name method from <code>MapFactory</code>
-	 * Note: will be removed with the next minor release
+	 * @deprecated use instead the same name method from <code>MapFactory</code> Note: will be
+	 *             removed with the next minor release
 	 */
 	@SuppressWarnings({ "unchecked", "rawtypes" })
 	public static <K, V> Map<K, V> newLazyTreeMap()
@@ -311,8 +307,8 @@ public final class MapExtensions
 	 *            the comparator
 	 *
 	 * @return The new {@link LazyMap}.
-	 * @deprecated use instead the same name method from <code>MapFactory</code>
-	 * Note: will be removed with the next minor release
+	 * @deprecated use instead the same name method from <code>MapFactory</code> Note: will be
+	 *             removed with the next minor release
 	 */
 	@SuppressWarnings({ "unchecked", "rawtypes" })
 	public static <K, V> Map<K, V> newLazyTreeMap(final Comparator<? super K> comparator)
@@ -330,8 +326,8 @@ public final class MapExtensions
 	 *            the generic type of the value
 	 *
 	 * @return The new {@link TreeMap}.
-	 * @deprecated use instead the same name method from <code>MapFactory</code>
-	 * Note: will be removed with the next minor release
+	 * @deprecated use instead the same name method from <code>MapFactory</code> Note: will be
+	 *             removed with the next minor release
 	 */
 	public static <K, V> Map<K, V> newTreeMap()
 	{
@@ -348,8 +344,8 @@ public final class MapExtensions
 	 * @param comparator
 	 *            the comparator
 	 * @return The new {@link TreeMap}.
-	 * @deprecated use instead the same name method from <code>MapFactory</code>
-	 * Note: will be removed with the next minor release
+	 * @deprecated use instead the same name method from <code>MapFactory</code> Note: will be
+	 *             removed with the next minor release
 	 */
 	public static <K, V> Map<K, V> newTreeMap(final Comparator<? super K> comparator)
 	{
