8f1aebe4ee350f22cc3ea353dc6781be65dcfebd
Asterios Raptis <asterios.raptis@gmx.net>
2018-06-13 19:05:16 +0200

    DateComparatorTest extends now from BaseComparatorTestCase

--- src/test/java/de/alpharogroup/collections/set/SetFactoryTest.java
+++ src/test/java/de/alpharogroup/collections/set/SetFactoryTest.java
@@ -75,36 +75,7 @@ public class SetFactoryTest
 		set = SetFactory.newHashSet("foo", "bar", "foo");
 		assertTrue(set.size() == 2);
 	}
-	
-	/**
-	 * Test for method {@link SetFactory#newLinkedHashSet(java.util.Collection, Object...)}
-	 */
-	@Test
-	public void testNewLinkedHashSetCollectionObjects()
-	{
-		Set<String> set = SetFactory.newLinkedHashSet();
-		assertTrue(set.size() == 0);
-		set.add("foo");
-		assertTrue(set.size() == 1);
-		set = SetFactory.newLinkedHashSet(ListFactory.newArrayList("foo", "fasel"), "foo", "bar", "foo");
-		assertTrue(set.size() == 3);
-		set = SetFactory.newLinkedHashSet(ListFactory.newArrayList(), "foo", "bar", "foo");
-		assertTrue(set.size() == 2);
 
-	}
-
-	/**
-	 * Test for method {@link SetFactory#newLinkedHashSet(Object...)}
-	 */
-	@Test
-	public void testNewLinkedHashSetObjects()
-	{
-		Set<String> set = SetFactory.newLinkedHashSet();
-		assertTrue(set.size() == 0);
-		set = SetFactory.newLinkedHashSet("foo", "bar", "foo");
-		assertTrue(set.size() == 2);
-	}
-	
 	/**
 	 * Test for method {@link SetFactory#newInsertionOrderSet(java.util.Collection, Object...)}
 	 */
@@ -115,7 +86,8 @@ public class SetFactoryTest
 		assertTrue(set.size() == 0);
 		set.add("foo");
 		assertTrue(set.size() == 1);
-		set = SetFactory.newInsertionOrderSet(ListFactory.newArrayList("foo", "fasel"), "foo", "bar", "foo");
+		set = SetFactory.newInsertionOrderSet(ListFactory.newArrayList("foo", "fasel"), "foo",
+			"bar", "foo");
 		assertTrue(set.size() == 3);
 		set = SetFactory.newInsertionOrderSet(ListFactory.newArrayList(), "foo", "bar", "foo");
 		assertTrue(set.size() == 2);
@@ -131,6 +103,36 @@ public class SetFactoryTest
 		Set<String> set = SetFactory.newInsertionOrderSet();
 		assertTrue(set.size() == 0);
 		set = SetFactory.newInsertionOrderSet("foo", "bar", "foo");
+		assertTrue(set.size() == 2);
+	}
+
+	/**
+	 * Test for method {@link SetFactory#newLinkedHashSet(java.util.Collection, Object...)}
+	 */
+	@Test
+	public void testNewLinkedHashSetCollectionObjects()
+	{
+		Set<String> set = SetFactory.newLinkedHashSet();
+		assertTrue(set.size() == 0);
+		set.add("foo");
+		assertTrue(set.size() == 1);
+		set = SetFactory.newLinkedHashSet(ListFactory.newArrayList("foo", "fasel"), "foo", "bar",
+			"foo");
+		assertTrue(set.size() == 3);
+		set = SetFactory.newLinkedHashSet(ListFactory.newArrayList(), "foo", "bar", "foo");
+		assertTrue(set.size() == 2);
+
+	}
+
+	/**
+	 * Test for method {@link SetFactory#newLinkedHashSet(Object...)}
+	 */
+	@Test
+	public void testNewLinkedHashSetObjects()
+	{
+		Set<String> set = SetFactory.newLinkedHashSet();
+		assertTrue(set.size() == 0);
+		set = SetFactory.newLinkedHashSet("foo", "bar", "foo");
 		assertTrue(set.size() == 2);
 	}
 
