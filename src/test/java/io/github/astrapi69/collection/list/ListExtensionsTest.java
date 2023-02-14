0081ad0d8244fbdde88239bbd8962d373dc810e0
asterios.raptis <asterios.raptis@gmx.net>
2023-02-14 17:55:10 +0100

    formatted with spotless 2

--- src/test/java/io/github/astrapi69/collection/list/ListExtensionsTest.java
+++ src/test/java/io/github/astrapi69/collection/list/ListExtensionsTest.java
@@ -24,10 +24,22 @@
  */
 package io.github.astrapi69.collection.list;
 
-import static org.testng.AssertJUnit.*;
+import static org.testng.AssertJUnit.assertEquals;
+import static org.testng.AssertJUnit.assertFalse;
+import static org.testng.AssertJUnit.assertNotNull;
+import static org.testng.AssertJUnit.assertNull;
+import static org.testng.AssertJUnit.assertTrue;
 import static org.testng.internal.junit.ArrayAsserts.assertArrayEquals;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
 import java.util.function.Function;
 
 import org.meanbean.test.BeanTester;
